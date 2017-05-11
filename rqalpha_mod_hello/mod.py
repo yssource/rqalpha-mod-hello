3e668efca6c0647853288d257bc25d502d4f5fb8
johnsonchak <359080235@qq.com>
2017-05-11 16:55:07 +0800

    first time push

---
+++ rqalpha_mod_hello/mod.py
@@ -0,0 +1,10 @@
+from rqalpha.interface import AbstractMod
+
+
+class HelloWorldMod(AbstractMod):
+    def start_up(self, env, mod_config):
+        print(">>> HelloWorldMod.start_up")
+
+    def tear_down(self, success, exception=None):
+        print(">>> HelloWorldMod.tear_down")
+
---
+++ setup.py
@@ -0,0 +1,36 @@
+from os.path import dirname, join
+from pip.req import parse_requirements
+
+from setuptools import (
+    find_packages,
+    setup,
+)
+
+
+with open(join(dirname(__file__), 'rqalpha_mod_hello/VERSION.txt'), 'rb') as f:
+    version = f.read().decode('ascii').strip()
+    """
+        作为一个合格的mod，应该要有版本号哦。
+    """
+setup(
+    name='rqalpha_mod_hello',  #mod的名字
+    version=version,
+    description='A mod for RQAlpha to say hello',
+    packages=find_packages(exclude=[]),
+    author='yourname',
+    author_email='your@email.com',
+    license='Apache License v2',
+    package_data={'': ['*.*']},
+    url=None,
+    install_requires=[str(ir.req) for ir in parse_requirements("requirements.txt", session=False)],#所需的运行环境
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
